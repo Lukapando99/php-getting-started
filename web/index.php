88ae0ae57f3c681af358ac1d3f192ac9a6e8c398
Lukapando99 <33448944+Lukapando99@users.noreply.github.com>
2017-11-07 11:11:45 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -7,6 +7,14 @@
 <?php
 echo "Hello World!";
 ?>
+  
+  <p style="color: #ff0000; font-family: trebuchet,
+  Tahoma,Verdana; font-size: 14px; font-weight: bold;"
+  align="center">
+    Un piccolo esempio di come disegnare
+      un cerchio con le librerie GD
+</p>
+<div align="center"><img src="immagine.php"></div>
 
 </body>
 </html>
